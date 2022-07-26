12003da3492deb029bc2dc98510264f8d1ddbde1
Svetlana K <kov_sv@mail.ru>
2022-07-26 19:24:49 +1000

    Изменение типа переменных на long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{1, 4, 3, 7, 9});
+        SalesManager salesManager = new SalesManager(new long[]{1, 4, 3, 7, 9});
         salesManager.max();
         System.out.println("Максимальное значение объема продаж: " + salesManager.max() + " ед.");
     }
