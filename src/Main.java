b12380379114f16ca52e6879ec870ec4e9e9dc88
Svetlana K <kov_sv@mail.ru>
2022-07-26 21:09:54 +1000

    Добавлен расчет обрезанного среднего значения объема продаж

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{1, 4, 3, 7, 9});
-        salesManager.max();
-        System.out.println("Максимальное значение объема продаж: " + salesManager.max() + " ед.");
+        SalesManager salesManager = new SalesManager (new int[]{2, 4, 3, 7, 9, 5, 13});
+        System.out.println("Максимальный объем продаж: " + salesManager.maxSale(salesManager.sales) + " ед.");
+        System.out.println("МИнимальный объем продаж " + salesManager.minSale(salesManager.sales) + " ед.");
+        System.out.println("Среднее обрезанное значение объема продаж: " + salesManager.averageValue(salesManager.sales));
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -6,13 +6,31 @@ public class SalesManager {
     }
 
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
-            if (sale > max) {
-                max = sale;
+    public int maxSale(int[] sales) {
+        int max = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] > max) {
+                max = sales[i];
             }
         }
         return max;
+    }
+    public int minSale(int[] sales) {
+        int min = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] < min) {
+                min = sales[i];
+            }
+        }
+        return min;
+    }
+    public int averageValue(int[] sales) {
+        int sum = sales[0];
+        int cutSum = 0;
+        for (int i = 1; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        cutSum = sum - maxSale(sales)-minSale(sales);
+        return ((cutSum)/(sales.length-2));
     }
 }
