e8f1564effb225f8dbf377b006edce4c384fd3cf
Svetlana K <kov_sv@mail.ru>
2022-07-26 21:19:16 +1000

    Внесены правки в метод определения максимального объема продаж

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -4,13 +4,11 @@ public class SalesManager {
     public SalesManager(int[] sales) {
         this.sales = sales;
     }
-
-
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
-            if (sale > max) {
-                max = sale;
+    public int maxSale(int [] sales) {
+        int max = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] > max) {
+                max = sales[i];
             }
         }
         return max;
